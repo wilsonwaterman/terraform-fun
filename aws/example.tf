5e2108d8ad6b7b91bff82553635664c43f79760d
Wilson Waterman <hwaterman@precima.com>
2020-05-25 15:59:00 -0400

    add more fun

--- aws/example.tf
+++ aws/example.tf
@@ -1,9 +1,19 @@
 provider "aws" {
     profile         = "default"
-    region          = "us-east-1"
+    region          = var.region
 }
 
 resource "aws_instance" "example" {
+    ami             = "ami-085925f297f89fce1"
+    instance_type   = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+    vpc             = true
+    instance        = aws_instance.example.id
+}
+
+resource "aws_instance" "another_one" {
     ami             = "ami-085925f297f89fce1"
     instance_type   = "t2.micro"
 }
